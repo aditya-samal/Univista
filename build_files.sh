56548e195f8d7c717c1e82586d3f9c284d18a4e3
aditya-samal <samaladitya2004@gmail.com>
2024-06-12 19:35:00 +0530

    fix

---
+++ build_files.sh
@@ -0,0 +1,7 @@
+echo "Migrating Database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
+
