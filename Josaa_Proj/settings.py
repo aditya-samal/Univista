807a448229c10d91cd6b7a7751b9090fcfe5dd8a
aditya-samal <samaladitya2004@gmail.com>
2024-06-12 21:07:03 +0530

    fix

--- Josaa_Proj/settings.py
+++ Josaa_Proj/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') 
-STATIC_URL = '/staticfiles_build/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
